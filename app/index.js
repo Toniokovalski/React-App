5b48c183181d6a060e4183aae38d8be327cbca9f
Toniokovalski <toniokovalski@gmail.com>
2018-03-10 21:08:54 +0200

    working commit

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./components/App');
 
-var container = document.getElementById('app')
+var container = document.getElementById('app');
 
 ReactDOM.render(
     <div>
